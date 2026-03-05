34ec5d6184db71a121d3bc2d66e2c81f4e37ee6c
AnteMilina <ante.milina@wearenotch.com>
2026-03-05 18:01:39 +0100

    add db sync controller form-data

--- src/main/java/com/kontron/endpoint/tester/cgms/controller/CgmsDbSyncController.java
+++ src/main/java/com/kontron/endpoint/tester/cgms/controller/CgmsDbSyncController.java
@@ -2,11 +2,9 @@ package com.kontron.endpoint.tester.cgms.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class CgmsDbSyncController {
 
-    @PostMapping("/dbsync")
-    public ResponseEntity<String> getDocumentEvent(@RequestBody String event) {
-        log.info("Received CGMS DB Sync event:\n{}", event);
-        return ResponseEntity.ok(event);
+    @PostMapping(value = "/dbsync", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> getDocumentEvent(@RequestPart("metadata") String metadata,
+                                                   @RequestPart(value = "oldXmlDocumentEvent", required = false) String oldXmlDocumentEvent,
+                                                   @RequestPart(value = "newXmlDocumentEvent", required = false) String newXmlDocumentEvent) {
+        log.info("Received CGMS DB Sync event:\nmetadata: {}\noldXmlDocumentEvent: {}\nnewXmlDocumentEvent: {}", metadata, oldXmlDocumentEvent, newXmlDocumentEvent);
+        return ResponseEntity.ok().build();
     }
 }
